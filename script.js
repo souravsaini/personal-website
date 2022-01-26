4b4bd84b2c1c53485704bd382f606309e569fddc
Sourav Saini <souravsaini@gmail.com>
2022-01-26 12:44:37 +0530

    added media queries, added contact and footer section

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var typed = new Typed(".typed", {
   backSpeed: 60,
   loop: true,
 });
+
+const btnNavElement = document.querySelector(".btn-mobile-nav");
+const headerElement = document.querySelector(".header");
+
+btnNavElement.addEventListener("click", function () {
+  headerElement.classList.toggle("nav-open");
+});
