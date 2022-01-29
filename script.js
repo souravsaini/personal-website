c2eb561972e990de7dee358fae19b0aa3297bb04
Sourav Saini <souravsaini@gmail.com>
2022-01-29 15:24:11 +0530

    Added animations

--- script.js
+++ script.js
@@ -11,3 +11,62 @@ const headerElement = document.querySelector(".header");
 btnNavElement.addEventListener("click", function () {
   headerElement.classList.toggle("nav-open");
 });
+
+//smooth scroll
+const links = document.querySelectorAll(".menu-link, .logo");
+console.log(links);
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //scroll to section
+    if (href !== "#" && href.startsWith("#")) {
+      const element = document.querySelector(href);
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile nav
+    if (link.classList.contains("menu-link")) {
+      headerElement.classList.toggle("nav-open");
+    }
+  });
+});
+
+const skillSection = document.querySelector(".section-skills");
+const skillBarsSection = document.querySelector(".skills-bars-box");
+const terraformSkill = document.querySelector(".terraform");
+const awsSkill = document.querySelector(".aws");
+const pythonSkill = document.querySelector(".python");
+const gitSkill = document.querySelector(".git");
+const htmlSkill = document.querySelector(".html");
+const cssSkill = document.querySelector(".css");
+
+window.addEventListener("scroll", () => {
+  let top = window.pageYOffset + window.innerHeight;
+  let isVisible;
+
+  //animation depends upon the sceen width
+  if (screen.width > 832) {
+    isVisible = top > skillSection.offsetTop + 200;
+  } else {
+    isVisible = top > skillBarsSection.offsetTop + 100;
+  }
+
+  if (isVisible) {
+    terraformSkill.classList.add("animate-terraform");
+    awsSkill.classList.add("animate-aws");
+    pythonSkill.classList.add("animate-python");
+    gitSkill.classList.add("animate-git");
+    htmlSkill.classList.add("animate-html");
+    cssSkill.classList.add("animate-css");
+  }
+});
